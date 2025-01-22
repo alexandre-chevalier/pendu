a68137ec3f3e1ac2afb16c783a78563763bce92d
alexandre <alexandre.chevalier@laplateforme.io>
2025-01-22 21:56:09 +0100

    program running from beginning to the end without graphic interfaceand no input control

--- pendu.py
+++ pendu.py
@@ -1,56 +1,45 @@
 import random
 
-
-
 ## fonction pour inserer un mot dans le fichier texte
 def insert():
-    insertion = input("veuillez entrez le mot que vous voulez ajoutez a la liste :")+'\n'
+    insertion = input("veuillez entrez le mot que vous voulez ajoutez a la liste :")+"\n"
     with open('mots.txt', 'a') as fichier:
         fichier.write(insertion)
 
 ## fonction pour choisir un mot aleatoire dans le fichier texte et qui retourne le mot
 def word_choice():
     with open('mots.txt', 'r') as word:
-        read = word.read().strip("\n")
-        print(read)
-        word = read[random.randint(0, len(read)-1)]
-        print(word)
-        return word
+            tab_words = [i.strip() for i in word]
+            return random.choice(tab_words)
+            
 
 def lose():
     print(f"vous avez perdu")
 
-
 ## fonction qui gere le jeu
 def game(scoring,essai,word):
-    mot = []
+    words = ["_" for i in word]
     wrong_letters = []
-
-    print(word)
-    for i in range(len(word)):
-            mot.append("-")
     while True:
-
-        if " ".join(mot) == word:
-            print("vtc")
-            return score(essai, scoring)
-        if essai == 11:
+        print("".join(words))
+        letter = input("veuillez entrez une lettre : ")
+        if essai == 11: 
             lose()
             break
-        print(mot)
-        lettre = input("veuillez entrez une lettre : ")
-        
-        print(f"vous avez {essai} erreur")
-            
-        if lettre in word:   
-            for i in range(len(word)):
-                if lettre == word[i]:
-                    mot[i] = lettre
-                    print(mot)
-        if lettre not in word:
-            essai += 1
-            wrong_letters.append(lettre)
-            print(wrong_letters)
+        else:
+            if letter in word:
+                for i in range(len(word)):
+                    if letter == word[i]:
+                        words[i] = letter
+            if letter not in word:
+                wrong_letters.append(letter)
+                print(wrong_letters)
+                essai+=1
+
+            if "".join(words) == word:
+                print("vtc")
+                score(essai, scoring)
+
 
 
 
@@ -64,13 +53,8 @@ def score(essai, scoring):
     elif essai >= 9 and essai <=10:
         scoring +=25    
     elif essai == 11:
-        scoring +=0
-
-    return message_victoire(essai, scoring)
-        
-
-
-                    
+        scoring =0
+    message_victoire(essai, scoring)         
                     
 def message_victoire(essai, scoring):
     print(f"vous avez trouver le mot mais vous avez fait {essai} erreurs,  ce qui vous fait un score de {scoring}")
--- test.py
+++ test.py
@@ -1,21 +1,37 @@
-with open('test.txt', 'r') as file:
-    for mot in file:
-        print(mot)
-
-
 import pygame
+import os
 from pygame.locals import *
 
 
-def screen():
+def screen1():
+
+    orange = [255,140,0]
+    r = Rect(300,400,400,75)
+    my_Font = pygame.font.SysFont("comicsansms", 16)
+    ng = "new game"
+
     pygame.display.set_caption("hello")
     screen = pygame.display.set_mode((1000,600))
+    wallpaper = pygame.image.load(os.path.join('image\cyberpunk.jpg')).convert()
+    pygame.Surface.blit(screen, wallpaper, (0,0))
+    menu = pygame.draw.rect(screen, orange, r,0)
+    ng_display = my_Font.render(ng, 1 , (255,255,255))
+    screen.blit(ng_display, (100,100))
 
+
+def screen2():
+    screen = pygame.display.set_mode((1000,600))
+    wallpaper = pygame.image.load(os.path.join('image\cyberpunk.jpg')).convert()
+    pygame.Surface.blit(screen, wallpaper, (0,0))
+    
+
+    
 def main():
     pygame.init()
+    pygame.font.init()
     run = True
+    screen1()
     while run:
-        screen()
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
